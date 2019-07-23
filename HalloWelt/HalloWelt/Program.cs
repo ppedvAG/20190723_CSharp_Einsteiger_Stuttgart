1b5044ce33a4b50dc3b49241e80870b09ee5b1c3
JoeVice <DanielWill@users.noreply.github.com>
2019-07-23 14:30:09 +0200

    Modul01 - Lab01

--- HalloWelt/HalloWelt/Program.cs
+++ HalloWelt/HalloWelt/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace HalloWelt
 {
@@ -11,19 +12,27 @@ namespace HalloWelt
         static void Main(string[] args)
         {
             #region Sprachgrundlagen Eingabe/Ausgabe
-
             //string einText = "Hallo Welt";
             //string meinName = "Daniel";
             //Console.WriteLine(einText + ", ich bin Daniel");
             //Console.WriteLine("{0} ich bin {1}", einText, meinName);
             //Console.WriteLine($"{einText} , ich bin Daniel");
-            Console.WriteLine("Bitte geben Sie Ihren Namen ein:");
-            //Console.WriteLine($"Hallo {Console.ReadLine()}");
-            string EingabeName = Console.ReadLine();
-            Console.WriteLine($"Hallo {EingabeName}");
-            Console.WriteLine("Bitte ein Alter angeben (Ganzzahl!): ");
-            sbyte EingabeAlter = Convert.ToSByte(Console.ReadLine());
-            Console.WriteLine($"Dein Alter ist: {EingabeAlter}");
+            //Console.WriteLine("Bitte geben Sie Ihren Namen ein:");
+            ////Console.WriteLine($"Hallo {Console.ReadLine()}");
+            //string EingabeName = Console.ReadLine();
+            //Console.WriteLine($"Hallo {EingabeName}");
+            //Console.WriteLine("Bitte ein Alter angeben (Ganzzahl!): ");
+            //sbyte EingabeAlter = Convert.ToSByte();
+            //long output;
+            //bool result = Int64.TryParse(Console.ReadLine(), out output);
+            //long eingabe2 = Int64.Parse(Console.ReadLine());
+            double ddtest = 5.55;
+            int test = (int)ddtest;
+            MessageBox.Show(Convert.ToString(test));
+
+            //Console.WriteLine($"Alter ist {eingabe2}");
+
+            //Console.WriteLine($"Dein Alter ist: {EingabeAlter}");
 
             #endregion Sprachgrundlagen Eingabe/Ausgabe
 

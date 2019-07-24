5fa79776673a137cb2bce6c952e4ea98bf3df219
JoeVice <DanielWill@users.noreply.github.com>
2019-07-24 13:40:39 +0200

    Enumeratoren & Switch-Case

---
+++ Enumeratoren/Enumeratoren/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeratoren
+{
+    // Enum-Deklaration im Namespace, um namensraumweit Zugriff zu gewährleisten
+    enum Mahlzeit { Frühstück = 1, Mittagessen, Abendessen, Mitternachtsnack };
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahlzeit aktuelleMahlzeit = Mahlzeit.Mittagessen;
+            string einString = "Hallo";
+
+            // Der Wert einer Variablen wird geprüft
+            switch (einString)
+            {
+                case "Hallo":
+                    // führe aus
+                    Console.WriteLine("Hallo steht in der Variablen");
+                    break;
+                default:
+                    break;
+            }
+
+
+            switch (aktuelleMahlzeit)
+            {
+                case Mahlzeit.Frühstück:
+                    break;
+
+                case Mahlzeit.Mittagessen:
+                    break;
+
+                case Mahlzeit.Abendessen:
+                    break;
+                // Typecasting INT -> Mahlzeit
+                case (Mahlzeit)(4):
+                    Console.WriteLine("Es gibt einen Mitternachtssnack");
+                    break;
+                default:
+                    break;
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Enumeratoren/Enumeratoren/Program2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeratoren
+{
+    class Program2
+    {
+        public void Test()
+        {
+            Mahlzeit einEssen = Mahlzeit.Mitternachtsnack;
+        }
+    }
+}

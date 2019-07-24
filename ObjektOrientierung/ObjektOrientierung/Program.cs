4e1d946d0cbc60a98f645a9ff875331d72e5bcec
JoeVice <DanielWill@users.noreply.github.com>
2019-07-24 16:48:32 +0200

    OOP: Werkzeuge

---
+++ ObjektOrientierung/ObjektOrientierung/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Werkzeuge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Erzeugen einer Objekt-Instanz anhand des Bauplans (Klasse: Prüfgerät)
+            Prüfgerät prüfgerät_TypA = new Prüfgerät();
+            prüfgerät_TypA.Hersteller = "Keysight";
+
+            // Erzeugen einer weiteren Objekt-Instanz anhand des Bauplans
+            Prüfgerät prüfgerät_TypB = new Prüfgerät();
+            // Ausführen der Methode "Einschalten" eines Prüfgerätes
+            prüfgerät_TypA.Einschalten();
+
+            prüfgerät_TypB.Einschalten();
+
+            // Ausgabe
+            Console.WriteLine(prüfgerät_TypA.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ObjektOrientierung/ObjektOrientierung/Prüfgerät.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Werkzeuge
+{
+    class Prüfgerät
+    {
+        #region Eigenschaften
+        public string Hersteller;
+        private DateTime BauDatum;
+        private int ID;
+        private string Modelbezeichnung;
+        #endregion
+
+        #region Konstruktor
+
+        #endregion
+
+        #region Methoden
+        public bool Einschalten()
+        {
+            Console.WriteLine("Prüfgerät wurde angeschaltet");
+            return true;
+        } 
+
+        public bool Ausschalten()
+        {
+            return true;
+        }
+
+        public float SpannungMessen()
+        {
+            return 0;
+        }
+        #endregion
+    }
+}

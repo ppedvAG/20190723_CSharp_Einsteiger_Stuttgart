303408a3dd159d9d15ee1cbcffc7ceff96099792
JoeVice <DanielWill@users.noreply.github.com>
2019-07-24 14:37:46 +0200

    Funktionen / out/ref Modul03 - Übung

---
+++ Funktionen/Funktionen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Funktionen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alter = 29;
+            Console.WriteLine($"Vor Ref-Aufruf: {alter}");
+            ändereAlter(ref alter, 40);
+            Console.WriteLine($"Nach Ref-Aufruf: {alter}");
+            Console.ReadLine();
+        }
+        public static void ändereAlter(ref int _irgendwas, int zuÄndern)
+        {
+            _irgendwas = zuÄndern;
+            //return true;
+        }
+
+    }
+}

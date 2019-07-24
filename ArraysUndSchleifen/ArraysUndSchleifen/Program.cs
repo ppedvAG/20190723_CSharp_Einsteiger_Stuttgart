e62892b04e5dd738ef25cf7d6936b01285263d04
JoeVice <DanielWill@users.noreply.github.com>
2019-07-24 09:48:45 +0200

    Arrays und Schleifen

---
+++ ArraysUndSchleifen/ArraysUndSchleifen/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysUndSchleifen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Arrays & Schleifen
+            int[] zahlen = new int[] { 10, 20, 30, 40, 50 };
+            int[,] zahlen2 = new int[,] { { 5, 6, 7 }, { 7, 8, 9 } };
+
+            Console.WriteLine($"{zahlen2[0,0]}");
+            Console.WriteLine($"{zahlen.Sum()}");
+            Console.WriteLine($"Arraylänge: {zahlen.Length}");
+
+            int i;
+            for (i = 0; i <= zahlen.Length; i++)
+            {
+                Console.WriteLine($"Aktuelle Zahl: {zahlen[i]}");
+            }
+            i = 0;
+            while (i < zahlen.Length)
+            {
+                // Operationen
+                Console.WriteLine($"{i}ter Durchlauf.");
+                i++;
+            }
+
+            do
+            {
+                // Operationen
+                Console.WriteLine($"{i}ter Durchlauf.");
+                i++;
+            } while (i < zahlen.Length);
+
+            foreach (int zahl in zahlen)
+            {
+                Console.WriteLine($"Aktuelle Zahl: {zahl}");
+                Console.WriteLine($"{zahl.GetType()}");
+            }
+
+            #endregion Arrays & Schleifen
+
+            Console.WriteLine("--- ENDE ---");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab01_MiniRechner_Console/Arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] zahlen1 = new int[1];
+            int[] zahlen3 = new int[] { 1, 2, 3 };
+            //int[50] zahlen4;
+
+            Array.Resize(ref zahlen1, 2);
+            zahlen1.SetValue(10, 0);
+            zahlen1[1] = 1000;
+
+            //int[,] mehrdim = new int[1, 1];
+
+            Console.WriteLine(zahlen1.Length);
+
+            //List<string> ZahlenListe = new List<string>();
+            //ZahlenListe.Add("Element 1");
+            //ZahlenListe.Add("Element 2");
+            //Console.WriteLine($"Index: {ZahlenListe.IndexOf("Element 1")}")
+
+            //int[] zahlen2 = zahlen1.Append(20).ToArray();
+
+            //zahlen[2] = 30;
+
+            //Console.WriteLine(zahlen1.GetLength(0));
+            //Console.WriteLine(zahlen2.Length);
+            //Console.WriteLine(zahlen1[0]);
+
+
+            Console.ReadLine();
+        }
+
+    }
+}

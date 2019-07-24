735787b1d29bacc09feaf0016eb043d220bc5ac5
JoeVice <DanielWill@users.noreply.github.com>
2019-07-24 14:40:08 +0200

    Funktion: params-Schlüsselwort

--- Funktionen/Funktionen/Program.cs
+++ Funktionen/Funktionen/Program.cs
@@ -16,11 +16,24 @@ namespace Funktionen
             ändereAlter(ref alter, 40);
             Console.WriteLine($"Nach Ref-Aufruf: {alter}");
             Console.ReadLine();
+
+            int meineSumme = bildeSumme(5, 5, 5, 6, 3, 5, 3);
         }
         public static void ändereAlter(ref int _irgendwas, int zuÄndern)
         {
             _irgendwas = zuÄndern;
             //return true;
+        }
+
+        // params: Dynamische Werte-Übergabe an die Funktion .. siehe Zeile 20!
+        public static int bildeSumme(params int[] summanden)
+        {
+            int summe = 0;
+            foreach (int item in summanden)
+            {
+                summe += item;
+            }
+            return summe;
         }
 
     }

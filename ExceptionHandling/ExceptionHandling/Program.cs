bc8e4de1252d7e627bee043eb83c53f6e2fb5d9f
JoeVice <DanielWill@users.noreply.github.com>
2019-07-26 10:23:56 +0200

    Modul09 - ExceptionHandling

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            START:
+            try
+            {
+                try
+                {
+                    int eingabe = int.Parse(Console.ReadLine());
+
+                } catch (Exception)
+                {
+                    throw;
+
+                    // throw;
+                } finally
+                {
+                    Console.WriteLine($"Bitte eine Zahl eingeben");
+
+                }
+                goto START;
+
+            } catch (Exception e)
+            {
+
+                Console.WriteLine($"{e.Message}");
+                Console.WriteLine($"{e.Data}");
+                Console.WriteLine($"{e.Source}");
+                Console.WriteLine($"{e.StackTrace}");
+                Console.WriteLine($"{e.TargetSite}");
+                throw e;
+            }
+            
+            // operationen
+        }
+    }
+}

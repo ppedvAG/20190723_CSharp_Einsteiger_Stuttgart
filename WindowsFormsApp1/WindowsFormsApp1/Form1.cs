4304403299d3f90985ac4cc63ad50946098ca678
JoeVice <DanielWill@users.noreply.github.com>
2019-07-26 14:45:01 +0200

    HalloWinForms

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Einfacher Klick!!!");
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
+
+        private void Button3_DoppelKlick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Doppelklick!!! OHNE MAUS");
+        }
+        private void Button3_MausDoppelKlick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Doppelklick!!! MIT MAUS");
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Form1 f1 = new Form1();
+            Form2 f2 = new Form2();
+
+            f1.Show();
+            f2.Show();
+
+            Application.Run();
+
+            //Application.Run(new Form2());
+        }
+    }
+}
